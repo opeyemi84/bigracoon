4e895cc4014048fccfc441144292b683f7ea780a
Opeyemi <opeadetola@gmail.com>
2024-05-05 22:25:12 -0400

    Create Button Component

---
+++ Components/Button.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+const Button = () => {
+  return (
+    <View style={StyleSheet.container}>
+      <TouchableOpacity style={styles.submitButton}>
+        <Text style={StyleSheet.text}>Button</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+  submitButton: {
+    backgroundColor: "#7a42f4",
+    borderRadius: 5,
+    width: "100%",
+    padding: 10,
+    margin: 15,
+    height: 40,
+  },
+  text: {
+    color: "white",
+    textAlign: "center",
+  },
+});
