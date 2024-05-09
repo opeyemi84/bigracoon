d8be3e57cdefc55e54c00a066431dd52d465e59a
Opeyemi <opeadetola@gmail.com>
2024-05-09 03:53:14 -0400

    New component--ForgotPassword.js -- devOpe

---
+++ Components/ForgotPassword.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+const ForgotPassword = () => {
+  return (
+    <View style={StyleSheet.container}>
+      <Text style={StyleSheet.title}>Password Reset</Text>
+    </View>
+  );
+};
